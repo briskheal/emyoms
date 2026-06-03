df2036fa9f686eaf7637991b52d5b1051b600428
J S DASH <emyrisbio@gmail.com>
2026-06-03 07:59:41 +0530

    chore: remove scratch/test files from repo

--- test_find_pending.js
+++
@@ -1,9 +0,0 @@
-const db = require('./models');
-
-async function checkPendingOrders() {
-    await db.sequelize.authenticate();
-    const orders = await db.Order.findAll({ where: { status: 'pending' }, include: ['items'] });
-    console.log(JSON.stringify(orders, null, 2));
-    process.exit(0);
-}
-checkPendingOrders();
