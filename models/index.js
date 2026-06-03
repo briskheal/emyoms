9d42ff3962a25379cba9946099fab022e06365a1
J S DASH <emyrisbio@gmail.com>
2026-06-03 07:09:45 +0530

    security: remove hardcoded DB credentials, use DATABASE_URL env var only

--- models/index.js
+++ models/index.js
@@ -11,39 +11,18 @@ pg.types.setTypeParser(1700, function(val) {
     return val === null ? null : parseFloat(val);
 });
 
-const NEON_HOST = "ep-green-mountain-aonjketm-pooler.c-2.ap-southeast-1.aws.neon.tech";
-const NEON_IP = "13.251.17.193";
-const NEON_USER = "neondb_owner";
-const NEON_PASS = "npg_bqJ3wQE8GWuX";
-const NEON_DB = "neondb";
 
-let sequelize;
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    logging: false,
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
+});
 
-if (process.env.DATABASE_URL) {
-    sequelize = new Sequelize(process.env.DATABASE_URL, {
-        dialect: 'postgres',
-        logging: false,
-        dialectOptions: {
-            ssl: {
-                require: true,
-                rejectUnauthorized: false
-            }
-        }
-    });
-} else {
-    sequelize = new Sequelize(`postgresql://${NEON_USER}:${NEON_PASS}@${NEON_IP}/${NEON_DB}`, {
-        dialect: 'postgres',
-        logging: false,
-        dialectOptions: {
-            ssl: {
-                require: true,
-                rejectUnauthorized: false,
-                servername: NEON_HOST,
-                checkServerIdentity: () => undefined
-            }
-        }
-    });
-}
 
 const db = {};
 db.Sequelize = Sequelize;
